98aa62d90435a13dc2a6b03be8e8a0144aaa58ff
atreyishukla <atreyishukla07@gmail.com>
2024-11-08 01:39:48 +0530

    fix

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
-
+import { Box, Flex, Input} from '@chakra-ui/react'
+import { Search } from 'lucide-react'
 const ChatPage = () => {
   return (
-    <div>ChatPage</div>
+    <Box
+			position={"absolute"}
+			left={"50%"}
+			w={{ base: "100%", md: "80%", lg: "750px" }}
+			p={4}
+			transform={"translateX(-50%)"}
+		>
+			<Flex
+				gap={4}
+				flexDirection={{ base: "column", md: "row" }}
+				maxW={{
+					sm: "400px",
+					md: "full",
+				}}
+				mx={"auto"}
+			>
+                <Flex flex={30}>
+                <Text fontWeight={700} className="bg-secondary">
+						Your Conversations
+					</Text>
+                    <form>
+                        <Flex>
+                            <Input placeholder='Search for a user'/>
+                            <Button size={"sm"}> 
+                                <Search/>
+                            </Button> 
+                        </Flex>
+                    </form>
+                </Flex>
+                <Flex flex={70}>MessageContainer</Flex>
+			</Flex>
+            </Box>
   )
 }
 
