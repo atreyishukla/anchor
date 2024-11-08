e3df8b8f45acde9229b34b300c990d8b50bae221
atreyishukla <atreyishukla07@gmail.com>
2024-11-09 00:39:19 +0530

    fix

--- frontend/src/pages/DonationPage.jsx
+++ frontend/src/pages/DonationPage.jsx
@@ -3,7 +3,7 @@ import {loadStripe} from '@stripe/stripe-js';
 import CheckoutForm from '../components/CheckoutForm';
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_51QIvGEKKDaEXrAi5AG3E0OfoPf2BwVTlr3MOgsNa8cY1uagGcgRQiaEmjnA06YKMr17iH6X7LoKCDsMbP0RMepzs008M6rzP4C');
+const stripePromise = loadStripe('sk_test_51QIvGEKKDaEXrAi5WZNgdQdTHEjZlGjHEeXarEEed8Q6pFVJJvh8TD4eOlfkb2eoXOJ8bi73514W5wHNh36QyhZx00itUxWnl3');
 
 export default function DonationPage() {
   const options = {
