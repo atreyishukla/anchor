3cd15c0e54e145b9490f27303968e19185b5a76c
atreyishukla <atreyishukla07@gmail.com>
2024-11-08 13:36:50 +0530

    fix

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,40 +1,8 @@
 import React from 'react'
-import { Box, Button, Flex, Input, Skeleton, SkeletonCircle, Text, useColorModeValue } from "@chakra-ui/react";
-import { Search } from 'lucide-react'
+
 const ChatPage = () => {
   return (
-    <Box
-			position={"absolute"}
-			left={"50%"}
-			w={{ base: "100%", md: "80%", lg: "750px" }}
-			p={4}
-			transform={"translateX(-50%)"}
-		>
-			<Flex
-				gap={4}
-				flexDirection={{ base: "column", md: "row" }}
-				maxW={{
-					sm: "400px",
-					md: "full",
-				}}
-				mx={"auto"}
-			>
-                <Flex flex={30}>
-                <Text fontWeight={700} className="bg-secondary">
-						Your Conversations
-					</Text>
-                    <form>
-                        <Flex>
-                            <Input placeholder='Search for a user'/>
-                            <Button size={"sm"}> 
-                                <Search/>
-                            </Button> 
-                        </Flex>
-                    </form>
-                </Flex>
-                <Flex flex={70}>MessageContainer</Flex>
-			</Flex>
-            </Box>
+    <div>ChatPage</div>
   )
 }
 
